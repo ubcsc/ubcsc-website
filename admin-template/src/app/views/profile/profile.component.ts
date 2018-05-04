47aea6bf5035edd8208b9e209b23ab8c04c7c3f5
austin047 <fuhaustin@gmail.com>
2018-05-04 04:43:15 +0100

    Added the angular Admin panel

---
+++ admin-template/src/app/views/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+
+@Component({
+  templateUrl: 'profile.component.html'
+})
+
+export class ProfileComponent {
+  
+  public saveProfilePictureModal;
+  public savePersonalInfoModal;
+}
